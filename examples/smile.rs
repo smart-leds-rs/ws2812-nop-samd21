3aa358beac81a08a84e9ea854a027b797d83a3f9
Paul Sajna <sajattack@gmail.com>
2019-01-01 16:39:47 -0800

    initial commit

---
+++ examples/smile.rs
@@ -0,0 +1,43 @@
+#![feature(asm)]
+#![no_std]
+#![no_main]
+
+extern crate circuit_playground_express;
+
+#[macro_use(entry)]
+extern crate cortex_m_rt;
+extern crate cortex_m;
+
+extern crate panic_abort;
+extern crate ws2812_nop_samd21;
+
+use circuit_playground_express::clock::GenericClockController;
+use circuit_playground_express::{CorePeripherals, Peripherals};
+
+use ws2812_nop_samd21::Ws2812;
+use smart_leds_trait::SmartLedsWrite;
+use smart_leds_trait::Color;
+
+entry!(main);
+
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+     
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+
+    let mut pins = circuit_playground_express::Pins::new(peripherals.PORT);
+    let neopixel_pin = pins.neopixel.into_push_pull_output(&mut pins.port);
+    let mut neopixel = Ws2812::new(neopixel_pin);
+
+    let yellow = Color {r: 0x01, g: 0x01, b: 0x00};
+    let off = Color {r: 0x00, g: 0x00, b: 0x00};
+    let smile = [yellow, off, yellow, yellow, yellow, yellow, yellow, yellow, off, yellow];  
+    neopixel.write(smile.iter().cloned());
+    loop {}
+}
---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+#![no_std]
+#![feature(asm)]
+
+use embedded_hal::digital::OutputPin;
+use smart_leds_trait::{Color, SmartLedsWrite};
+
+pub struct Ws2812<P: OutputPin> {
+    pin: P,
+}
+
+impl <P: OutputPin> Ws2812<P> {
+    pub fn new(pin: P) -> Ws2812<P> {
+        Ws2812 { pin: pin }
+    }
+    fn write_byte(&mut self, data: u8) {
+        let mut bitmask: u8 = 0x80;
+        while bitmask != 0 {
+            self.pin.set_high();
+            unsafe{ 
+                asm!("nop; nop;");
+            }
+            if data & bitmask != 0 {
+                unsafe{ 
+                    asm!("nop; nop; nop; nop; nop; nop; nop;")
+                }
+                self.pin.set_low();
+            } else {
+                self.pin.set_low();
+                unsafe{ 
+                    asm!("nop; nop;");
+                }
+            }
+            bitmask >>= 1;
+        }
+        unsafe {
+            asm!("nop; nop; nop; nop; nop;");
+        }
+    }
+}
+
+impl<P> SmartLedsWrite for Ws2812<P>
+where 
+    P: OutputPin,
+{
+    type Error = ();
+    fn write<T>(&mut self, iterator: T) -> Result<(), ()>
+    where
+        T: Iterator<Item = Color>,
+    {
+        for item in iterator {
+           self.write_byte(item.g);
+           self.write_byte(item.r);
+           self.write_byte(item.b);
+        }
+        Ok(())
+    }
+}
