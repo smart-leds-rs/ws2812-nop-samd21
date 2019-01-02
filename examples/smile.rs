ebea235174f7d8011c893052a8d778497601bff4
Paul Sajna <sajattack@gmail.com>
2019-01-01 22:12:26 -0800

    use newly added color definitions in example

--- examples/smile.rs
+++ examples/smile.rs
@@ -17,6 +17,7 @@ use circuit_playground_express::{Peripherals};
 use ws2812_nop_samd21::Ws2812;
 use smart_leds_trait::SmartLedsWrite;
 use smart_leds_trait::Color;
+use smart_leds::colors::YELLOW;
 
 entry!(main);
 
@@ -34,9 +35,8 @@ fn main() -> ! {
     let neopixel_pin = pins.neopixel.into_push_pull_output(&mut pins.port);
     let mut neopixel = Ws2812::new(neopixel_pin);
 
-    let yellow = Color {r: 0x01, g: 0x01, b: 0x00};
-    let off = Color {r: 0x00, g: 0x00, b: 0x00};
-    let smile = [yellow, off, yellow, yellow, yellow, yellow, yellow, yellow, off, yellow];  
+    let off = Color::default();
+    let smile = [YELLOW, off, YELLOW, YELLOW, YELLOW, YELLOW, YELLOW, YELLOW, off, YELLOW];  
     neopixel.write(smile.iter().cloned()).unwrap();
     loop {}
 }
